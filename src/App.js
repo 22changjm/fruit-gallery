f00f4137f7622f206e23c459cad3fc191f814811
Michael Chang <22mchang@gmail.com>
2021-05-03 23:31:52 -0700

    deploy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import Images from './Images'
+import React, {useState, useEffect} from 'react'
+import styled from 'styled-components'
+import { Modal } from './components/Modal'
+import { GlobalStyle } from './globalStyles'
+import { MdSystemUpdate } from 'react-icons/md'
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  margin: 40px;
+  flex-direction: column;
+`
+const Header = styled.h1`
+  font-family: 'Nunito', sans-serif;
+`
+
+const Button = styled.button`
+border-radius: 10px;
+border: solid;
+cursor: pointer;
+position: relative;
+display: block;
+border-width: thick;
+&:hover {
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.35);
+}
+`
+
+const Fruit = styled.img`
+width: 21em;
+height: 21em;
+object-fit: cover;
+cursor: pointer;
+border-radius: 10px;
+border: none;
+`
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, minmax(150px, 293px));
+  justify-content: center;
+  grid-gap: 28px;
+`
+
 
 function App() {
+  const fruitVals = ['Apple', 'Apricot', 'Banana', 'Blueberry', 'Cherry', 'Guava', 'Lemon', 'Mango', 'Orange', 'Pear', 'Pineapple', 'Raspberry', 'Strawberry', 'Tomato', 'Watermelon'];
+  const [showModal, setShowModal] = useState(false);
+  const [imageID, setImageID] = useState(0);
+  const [info, setInfo] = useState({})
+
+  const retrieveData = async () => {
+    const response = await fetch(`https://pacific-scrubland-02722.herokuapp.com/https://fruityvice.com/api/fruit/${fruitVals[imageID]}`);
+    const data = await response.json();
+    setInfo(data);
+    setShowModal(prev=> !prev)
+  }
+
+  const openModal = (event) => {
+      setImageID(parseInt(event.currentTarget.id));
+      retrieveData()
+    };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Container>
+      <Header>FRUIT GALLERY</Header>
+      <Grid>
+        <Button id="0" onClick={openModal}> <Fruit src={Images[0]}/></Button> 
+        <Button id="1" onClick={openModal}> <Fruit src={Images[1]}/></Button>
+        <Button id="2" onClick={openModal}> <Fruit src={Images[2]}/></Button>
+        <Button id="3" onClick={openModal}> <Fruit src={Images[3]}/></Button> 
+        <Button id="4" onClick={openModal}> <Fruit src={Images[4]}/></Button>
+        <Button id="5" onClick={openModal}> <Fruit src={Images[5]}/></Button>
+        <Button id="6" onClick={openModal}> <Fruit src={Images[6]}/></Button> 
+        <Button id="7" onClick={openModal}> <Fruit src={Images[7]}/></Button>
+        <Button id="8" onClick={openModal}> <Fruit src={Images[8]}/></Button>
+        <Button id="9" onClick={openModal}> <Fruit src={Images[9]}/></Button> 
+        <Button id="10" onClick={openModal}> <Fruit src={Images[10]}/></Button>
+        <Button id="11" onClick={openModal}> <Fruit src={Images[11]}/></Button>
+      </Grid>
+      <Modal showModal={showModal} setShowModal={setShowModal} imageID={imageID} info={info}/>
+      <GlobalStyle />
+    </Container>
+    </>
   );
 }
 
---
+++ src/components/Modal.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import styled from 'styled-components'
+import {MdClose} from 'react-icons/md'
+import Images from '../Images'
+
+
+const Background = styled.div`
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ModalWrapper = styled.div`
+  width: 800px;
+  height: 500px;
+  box-shadow: 0 5px 16px rgba(0, 0, 0, 0.2);
+  background: #fff;
+  color: #000;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  position: relative;
+  z-index: 10;
+  border-radius: 10px;
+  align-items: center;
+  justify-items: center;
+`;
+
+const ModalImg = styled.img`
+    width: 20em;
+    height: 20em;
+    object-fit: cover;
+    border-radius: 10px
+`;
+
+const ModalContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  line-height: 1.8;
+  color: #141414;
+  p {
+    margin-bottom: 1rem;
+  }
+  button {
+    padding: 10px 24px;
+    background: #141414;
+    color: #fff;
+    border: none;
+  }
+`;
+
+const CloseModalButton = styled(MdClose)`
+  cursor: pointer;
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  width: 32px;
+  height: 32px;
+  padding: 0;
+  z-index: 10;
+`;
+
+const fruitVals = ['Apple', 'Apricot', 'Banana', 'Blueberry', 'Cherry', 'Guava', 'Lemon', 'Mango', 'Orange', 'Pear', 'Pineapple', 'Raspberry', 'Strawberry', 'Tomato', 'Watermelon'];
+
+export const Modal = ({showModal, setShowModal, imageID, info}) => {
+    return (
+        <>
+        {showModal ? (
+            <Background>
+                <ModalWrapper showModal={showModal}>
+                    <ModalImg src={Images[imageID]}/>
+                    <ModalContent>
+                        <h1>{fruitVals[imageID]}</h1>
+                        <p>Order: {info.order}</p>
+                        <p>Family: {info.family}</p>
+                        <p>Genus: {info.genus}</p> 
+
+                        <h2>Nutrition Facts per 100 grams </h2>
+                        <ul>
+                            <li> {info.nutritions.calories} Calories </li>
+                            <li>{info.nutritions.carbohydrates} grams of Carbohydrates </li>
+                            <li>{info.nutritions.protein} grams of Protein </li>
+                            <li> {info.nutritions.fat} grams of Fat </li>
+                            <li> {info.nutritions.sugar} grams of Sugar </li>
+                        </ul>
+                        
+                    </ModalContent>
+                    <CloseModalButton aria-label='Close modal' onClick={() => setShowModal (prev => !prev)} />
+                </ModalWrapper> 
+            </Background>
+        ) : null}
+        </>
+      )
+};
